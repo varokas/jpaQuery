76773de73cd82ea5b294dc2e16b41b5008a995af
Varokas Panusuwan <varokas@panusuwan.com>
2012-02-21 18:13:11 -0800

    Generate from JBehave Archetype

---
+++ src/main/java/com/huskycode/steps/MySteps.java
@@ -0,0 +1,10 @@
+package com.huskycode.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class MySteps {
+    
+}

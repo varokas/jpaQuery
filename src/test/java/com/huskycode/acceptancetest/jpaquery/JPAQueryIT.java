8e8b138680c375e02c66612fdce602945245b40e
Varokas Panusuwan <panusuwan@ridacto.com>
2012-03-26 23:30:49 -0700

    create blank integration test

---
+++ src/test/java/com/huskycode/acceptancetest/jpaquery/JPAQueryIT.java
@@ -0,0 +1,13 @@
+package com.huskycode.acceptancetest.jpaquery;
+
+import org.junit.Test;
+
+/**
+ * @author Varokas Panusuwan
+ */
+public class JPAQueryIT {
+    @Test
+    public void testTest() {
+        //TODO: Make acceptance test
+    }
+}

256fea142d3310b0a74e8a988e5e180bc79a3f63
Varokas Panusuwan <varokas@panusuwan.com>
2012-04-17 09:22:00 -0700

    Add missing class files.

---
+++ src/test/java/com/huskycode/jpaquery/testmodel/ClassA_.java
@@ -0,0 +1,12 @@
+package com.huskycode.jpaquery.testmodel;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(ClassA.class)
+public abstract class ClassA_ extends com.huskycode.jpaquery.testmodel.BaseClass_ {
+
+
+}
+
---
+++ src/test/java/com/huskycode/jpaquery/testmodel/ClassB_.java
@@ -0,0 +1,12 @@
+package com.huskycode.jpaquery.testmodel;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(ClassB.class)
+public abstract class ClassB_ extends com.huskycode.jpaquery.testmodel.BaseClass_ {
+
+
+}
+

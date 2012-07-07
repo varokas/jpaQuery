016aa254355b9484b67d8fe6bd4c78e291c9c427
Varokas Panusuwan <varokas@panusuwan.com>
2012-07-07 15:14:18 -0700

    Implements CommandNodeFactory

---
+++ src/main/java/com/huskycode/jpaquery/command/CommandNodeFactory.java
@@ -0,0 +1,75 @@
+package com.huskycode.jpaquery.command;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The way to create Command Node
+ * 
+ * @author Varokas Panusuwan
+ */
+public class CommandNodeFactory {
+	public static CommandNode n(Class<?> entity) {
+		return new CommandNodeImpl(entity, new ArrayList<CommandNode>());
+	}
+	
+	public static CommandNode n(Class<?> entity, CommandNode... children) {
+		return new CommandNodeImpl(entity, Arrays.asList(children));
+	}
+	
+	/**
+	 * Implementation of a Command Node
+	 */
+	private static class CommandNodeImpl implements CommandNode {
+		private final Class<?> entity;
+		private final List<CommandNode> children;
+		
+		public CommandNodeImpl(Class<?> entity, List<CommandNode> children) {
+			this.entity = entity;
+			this.children = children;
+		}
+		
+		@Override
+		public Class<?> getEntity() {
+			return entity;
+		}
+
+		@Override
+		public List<CommandNode> getChildren() {
+			return children;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((children == null) ? 0 : children.hashCode());
+			result = prime * result + ((entity == null) ? 0 : entity.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			CommandNodeImpl other = (CommandNodeImpl) obj;
+			if (children == null) {
+				if (other.children != null)
+					return false;
+			} else if (!children.equals(other.children))
+				return false;
+			if (entity == null) {
+				if (other.entity != null)
+					return false;
+			} else if (!entity.equals(other.entity))
+				return false;
+			return true;
+		}
+	}
+}
---
+++ src/test/java/com/huskycode/jpaquery/command/CommandNodeFactoryTest.java
@@ -0,0 +1,37 @@
+package com.huskycode.jpaquery.command;
+
+import static com.huskycode.jpaquery.command.CommandNodeFactory.n;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.huskycode.jpaquery.testmodel.pizza.Address;
+import com.huskycode.jpaquery.testmodel.pizza.Customer;
+import com.huskycode.jpaquery.testmodel.pizza.Customer_;
+
+public class CommandNodeFactoryTest {
+	private CommandNode commandNode;
+	
+	@Test
+	public void testCreateOneNode() {
+		commandNode = n(Address.class);
+		assertEquals(Address.class, commandNode.getEntity());
+		assertThat(commandNode.getChildren().size(), is(0));
+	}
+	
+	@Test
+	public void testCreateNodeWithChildren() {
+		commandNode = n(Address.class,
+			n(Customer.class)
+		);
+
+		assertEquals(Address.class, commandNode.getEntity());
+		assertThat(commandNode.getChildren().size(), is(1));
+		assertThat(commandNode.getChildren().get(0), is(n(Customer.class)));
+	}
+
+
+}

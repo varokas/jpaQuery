c8a4ef3cb52d9de0a56222bca976c9c000a714a8
Varokas Panusuwan <varokas@panusuwan.com>
2012-07-07 15:57:23 -0700

    Improve Integration Test

--- src/test/java/com/huskycode/acceptancetest/jpaquery/JPAQueryIT.java
+++ src/test/java/com/huskycode/acceptancetest/jpaquery/JPAQueryIT.java
@@ -1,10 +1,16 @@
 package com.huskycode.acceptancetest.jpaquery;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -13,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.huskycode.jpaquery.AbstractEntityManagerWired;
 import com.huskycode.jpaquery.JPAQueryContext;
 import com.huskycode.jpaquery.testmodel.pizza.Address;
+import com.huskycode.jpaquery.testmodel.pizza.Customer;
+import com.huskycode.jpaquery.testmodel.pizza.Employee;
 import com.huskycode.jpaquery.testmodel.pizza.PizzaOrder;
 import com.huskycode.jpaquery.testmodel.pizza.deps.PizzaDeps;
 import com.huskycode.jpaquery.types.tree.PersistedResult;
@@ -28,21 +36,53 @@ public class JPAQueryIT extends AbstractEntityManagerWired {
     @Before
     public void before() {
     	pizzaDeps = new PizzaDeps();
+    	context = JPAQueryContext.newInstance(entityManager, pizzaDeps.getDeps());
     }
 	
 	@Test
     public void testCreateClassWithNoDeps() {
-        context = JPAQueryContext.newInstance(entityManager, pizzaDeps.getDeps());
-        
         PersistedResult result = context.create(Address.class);
-        assertThat(result, is(not(nullValue())));
+        
+        assertThat(result.getPersistedObjects().size(), is(1));
+        Object persistedObj = result.getPersistedObjects().get(0);
+        
+        assertThat(persistedObj, is(instanceOf(Address.class)));
+        assertThat(((Address)persistedObj).getAddressId(), is(not(nullValue())));
     }
 	
 	@Test 
-    public void testCreateClassWithDeps() {
-        context = JPAQueryContext.newInstance(entityManager, pizzaDeps.getDeps());
-        
+    public void testCreateClassWithDeps() {        
         PersistedResult result = context.create(PizzaOrder.class);
-        assertThat(result, is(not(nullValue())));
+        
+        ClassMap resultMap = mapByClass(result.getPersistedObjects());
+        
+        Address address = resultMap.getForClass(Address.class);
+        Employee employee = resultMap.getForClass(Employee.class);
+        Customer customer = resultMap.getForClass(Customer.class);
+        PizzaOrder pizzaOrder = resultMap.getForClass(PizzaOrder.class);
+        
+        assertThat(employee.getEmployeeAddressId(), is(address.getAddressId()));
+        assertThat(customer.getCustomerAddressId(), is(address.getAddressId()));
+        assertThat(pizzaOrder.getCustomerId(), is(customer.getCustomerId()));
+        assertThat(pizzaOrder.getDeliveredByEmployeeId(), is(employee.getEmployeeId()));
+        assertThat(pizzaOrder.getTakenByEmployeeId(), is(employee.getEmployeeId()));
     }
+	
+	public class ClassMap extends HashMap<Class<?>, Object> {
+		private static final long serialVersionUID = 1L;
+	
+		@SuppressWarnings("unchecked")
+		public <E> E getForClass(Class<E> clazz) {
+			return (E) this.get(clazz);
+		}
+	}
+	
+	private ClassMap mapByClass(List<Object> objects) {
+		ClassMap map = new ClassMap();
+		for(Object obj : objects) {
+			map.put(obj.getClass(), obj);
+		}
+		
+		return map;
+	}
 }

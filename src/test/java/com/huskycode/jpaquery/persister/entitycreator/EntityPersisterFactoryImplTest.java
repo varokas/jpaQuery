ed21051b7c05b3e0c06f35ac822961b22501e415
Varokas Panusuwan <varokas@panusuwan.com>
2012-07-21 18:27:34 -0700

    Modify EntityPersisterFactory to return EnumTablePersister when the processing EntityNode has class in the Enum table set

--- src/test/java/com/huskycode/jpaquery/persister/entitycreator/EntityPersisterFactoryImplTest.java
+++ src/test/java/com/huskycode/jpaquery/persister/entitycreator/EntityPersisterFactoryImplTest.java
@@ -2,11 +2,9 @@ package com.huskycode.jpaquery.persister.entitycreator;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,28 +12,37 @@ import org.mockito.Mockito;
 
 import com.huskycode.jpaquery.DependenciesDefinition;
 import com.huskycode.jpaquery.DepsBuilder;
-import com.huskycode.jpaquery.link.Link;
 import com.huskycode.jpaquery.types.tree.EntityNode;
 
 public class EntityPersisterFactoryImplTest {
 
+	private Class<Integer> aClass = Integer.class;
 	private EntityPersisterFactoryImpl factory;
-	private DependenciesDefinition anyDeps;
 	private EntityNode anyNode;
 	private EntityManager em;
 
 	@Before
 	public void before() {
 		factory = new EntityPersisterFactoryImpl();
-		anyNode = EntityNode.newInstance(Integer.class);
-		anyDeps = new DepsBuilder().build();
+		anyNode = EntityNode.newInstance(aClass);
 		em = Mockito.mock(EntityManager.class);
 	}
 	
 	@Test
-	public void testCreateEntityPersisterAlwaysReturnNewRowEntityPersister() {
-		Assert.assertThat(factory.createEntityPersister(anyNode, anyDeps, em),
+	public void testCreateEntityPersisterReturnNewRowEntityPersisterIfNotInSetOfEnumTable() {
+		DependenciesDefinition depsWithoutEnumTable = new DepsBuilder().build();
+		
+		Assert.assertThat(factory.createEntityPersister(anyNode, depsWithoutEnumTable, em),
 				is(instanceOf(NewRowEntityPersister.class)));
+	}
+	
+	@Test
+	public void testCreateEntityPersisterReturnNewRowEntityPersister() {
+		DependenciesDefinition depsWithMatchingEnumTable = 
+				new DepsBuilder().withEnumTable(aClass).build();
+		
+		Assert.assertThat(factory.createEntityPersister(anyNode, depsWithMatchingEnumTable, em),
+				is(instanceOf(EnumTableEntityPersister.class)));
 	}
 
 }

c090ebe56773a24362ba68dd113be1254649272e
tawan <surachat@cobalt.com>
2014-08-17 18:07:05 -0700

    List Util to create list from iterable

--- src/main/java/com/huskycode/jpaquery/util/ListUtil.java
+++ src/main/java/com/huskycode/jpaquery/util/ListUtil.java
@@ -1,6 +1,7 @@
 package com.huskycode.jpaquery.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -15,4 +16,11 @@ public class ListUtil {
         return result;
     }
 
+    public static <T> List<T> from(Iterable<T> values) {
+        List<T> result = new ArrayList<T>();
+        for (T v : values) {
+            result.add(v);
+        }
+        return result;
+    }
 }
